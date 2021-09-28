8ea013766e5de99b37b74424b12e77b608a5e115
Pengolod <80605795+pengolod@users.noreply.github.com>
2021-09-28 18:46:37 -0500

    Add logging to build script, add search & discord link (#1)
    * Add logging to build script
    
    * Add additional. config for search & discord chat links!

--- build.js
+++ build.js
@@ -1,36 +1,52 @@
 const yaml = require("js-yaml");
 const fs = require("fs");
 const { execSync } = require("child_process");
+const { green } = require("kleur/colors");
 
+console.log(green("Installing packwiz..."));
 execSync("go install github.com/comp500/packwiz@latest");
+
+console.log(green("Outputting packwiz cli docs..."));
 execSync("packwiz utils markdown --dir=docs/reference/commands", {
-	env: {...process.env, PATH: `${process.env.GOPATH}/bin:${process.env.PATH}`}
+  env: {
+    ...process.env,
+    PATH: `${process.env.GOPATH}/bin:${process.env.PATH}`,
+  },
 });
 
+console.log(green("Massaging packwiz cli docs into mkdoc compatible form..."));
 let mkdocsConfig = yaml.load(fs.readFileSync("mkdocs-base.yml", "utf8"));
-Promise.all(mkdocsConfig.nav.map(async el => {
-	if (el.Reference != null) {
-		await Promise.all(el.Reference.map(async el => {
-			if (el.Commands != null) {
-				el.Commands = [];
+Promise.all(
+  mkdocsConfig.nav.map(async (el) => {
+    if (el.Reference != null) {
+      await Promise.all(
+        el.Reference.map(async (el) => {
+          if (el.Commands != null) {
+            el.Commands = [];
 
-				const files = await fs.promises.readdir("./docs/reference/commands/");
-				for (const file of files) {
-					let ref = {};
-					let name = file.slice(0, -3).replace(/_/g, " ");
-					if (name == "overview") {
-						name = "Overview";
-					}
-					ref[name] = "reference/commands/" + file;
-					el.Commands.push(ref);
-				}
-			}
-		}));
-	}
-	return el;
-})).then(nav => {
-	mkdocsConfig.nav = nav;
-	fs.writeFileSync("mkdocs.yml", yaml.dump(mkdocsConfig));
+            const files = await fs.promises.readdir(
+              "./docs/reference/commands/"
+            );
+            for (const file of files) {
+              let ref = {};
+              let name = file.slice(0, -3).replace(/_/g, " ");
+              if (name == "overview") {
+                name = "Overview";
+              }
+              ref[name] = "reference/commands/" + file;
+              el.Commands.push(ref);
+            }
+          }
+        })
+      );
+    }
+    return el;
+  })
+).then((nav) => {
+  mkdocsConfig.nav = nav;
+  fs.writeFileSync("mkdocs.yml", yaml.dump(mkdocsConfig));
 
-	execSync("mkdocs build");
+  console.log(green("Building docs!"));
+  execSync("mkdocs build");
+  console.log(green("Successfully built docs!"));
 });
